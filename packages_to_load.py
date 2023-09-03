052be14dd253488c4aa1369577a05ff7c7b64496
ataman <atamanenkoalex90@gmail.com>
2023-09-04 00:50:09 +0300

    Init

---
+++ packages_to_load.py
@@ -0,0 +1,19 @@
+def which_packages_to_load(truck_space: int, packages: list, reserve: int = 30):
+    target_space = truck_space - reserve
+    if target_space >= 0 and len(packages) > 1:
+        best_order = [0, 0]
+        packages.sort()
+        left_index = 0
+        right_index = len(packages) - 1
+        while left_index < right_index:
+            current_packages = [packages[left_index], packages[right_index]]
+            if sum(current_packages) == target_space:
+                return current_packages
+            elif sum(current_packages) < target_space:
+                if sum(current_packages) > sum(best_order):
+                    best_order = current_packages
+                left_index += 1
+            else:
+                right_index -= 1
+        return best_order
+    return []
---
+++ tests.py
@@ -0,0 +1,35 @@
+import unittest
+
+from packages_to_load import which_packages_to_load
+
+
+class TestPackagesToLoad(unittest.TestCase):
+    def test_full_1(self):
+        actual = which_packages_to_load(110, [20, 70, 90, 30, 60, 110])
+        expected = [20, 60]
+        self.assertEqual(actual, expected)
+
+    def test_full_2(self):
+        actual = which_packages_to_load(130, [90, 80, 30, 20, 9, 19])
+        expected = [20, 80]
+        self.assertEqual(actual, expected)
+
+    def test_not_full(self):
+        actual = which_packages_to_load(110, [18, 70, 90, 30, 58, 110])
+        expected = [18, 58]
+        self.assertEqual(actual, expected)
+
+    def test_full_truck(self):
+        actual = which_packages_to_load(20, [1, 2])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_one_package_in_storage(self):
+        actual = which_packages_to_load(100, [1])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_empty_storage(self):
+        actual = which_packages_to_load(100, [])
+        expected = []
+        self.assertEqual(actual, expected)
